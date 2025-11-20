63b356104a9b0e86f7956c764c14b47732fe1a4e
jhonherikgc <jhonherik006@gmail.com>
2025-11-20 18:07:32 +0000

    :zap: perf

--- src/loginPage.tsx
+++ src/loginPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles/loginPage.css';
+import GoogleIcon from '@mui/icons-material/Google';
+import Button from '@mui/material/Button';
 
 const LoginPage: React.FC = () => {
   return (
@@ -9,10 +11,40 @@ const LoginPage: React.FC = () => {
         <div className="container-maior">
           <div className="login-container">
             <h1 className="login-title">Inicie sessão</h1>
+            <div className="separator" />
 
-            <input type="text" className="usuario-input" placeholder="Usuário" />
+            <input type="text" className="firstName-input" placeholder="Primeiro nome" />
+            <input type="text" className="lastName-input" placeholder="Último nome" />
+            <input type="text" className="email-input" placeholder="Email" />
             <input type="password" className="password-input" placeholder="Senha" />
 
+            {/* alinhamento: remember-me à esquerda e forgot-password à direita */}
+            <div className="actions-row">
+              <div className="remember-me">
+                <input type="checkbox" id="rememberMe" />
+                <label htmlFor="rememberMe">Lembrar-me</label>
+              </div>
+
+              <div className="forgot-password">
+                Esqueceu a senha?
+              </div>
+            </div>
+            
+            {/* botão de login com Google (acessível e interativo) */}
+            <div className="login-google">
+              <Button
+                variant="outlined"
+                className="google-login-button"
+                startIcon={<GoogleIcon className="google-icon" />}
+                onClick={() => {
+                  // coloque aqui a chamada para OAuth / redirect
+                  console.log('Entrar com Google');
+                }}
+                aria-label="Entrar com Google"
+              >
+                Entrar com Google
+              </Button>
+            </div>
             <div className="button-row">
               <input type="submit" className="login-button" value="Log in" />
               <input type="button" className="create-account-button" value="Criar conta" />
