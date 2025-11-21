447aaa3a20e9aabc772ddf48488e57aa2f8d2cfc
jhonherikgc <jhonherik006@gmail.com>
2025-11-21 17:06:22 +0000

    :zap: perf + new page component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import LoginPage from './loginPage'
+import CreateAccount from './createAccount'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LoginPage />
+    <CreateAccount />
   </StrictMode>,
 )
